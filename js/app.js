9c14b5466d97f628693871852e8d37ae8c18f668
Gabriel Nunes Pedroso <64169450+gnpedroso@users.noreply.github.com>
2020-08-25 19:23:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,82 +1,42 @@
-(function(){
-    
-    //search by clicking
-    let $tabs = document.querySelectorAll('.tabs_wrap ul li');
-    
-   let $reserva = document.querySelectorAll('.reserva')
-   let $granReserva = document.querySelectorAll('.granReserva')
-   let $specialCollection = document.querySelectorAll('.specialCollection')
-   let $all = document.querySelectorAll('.list-unstyled')
+let $tabs = document.querySelectorAll('.tabs_wrap ul li');
 
-   $tabs.forEach(tabs =>{
-       tabs.addEventListener('click', function(){
-           $tabs.forEach(tabs =>{
-               tabs.classList.remove('active')
-           })
-        
-        tabs.classList.add('active');
-        
-        const tabVal = tabs.getAttribute('data-tabs');
-        
+let $reserva = document.querySelectorAll('.reserva')
+let $granReserva = document.querySelectorAll('.granReserva')
+let $specialCollection = document.querySelectorAll('.specialCollection')
+let $all = document.querySelectorAll('.list-unstyled')
 
-        $all.forEach(all =>{
-            all.style.display = 'none'
-        })
 
-        $reserva.forEach(reserva =>{
-            if(tabVal === 'reserva'){
-                reserva.style.display = 'block'
-            }
-        })
+$tabs.forEach(tabs => {
+    tabs.addEventListener('click', (ev) => {
+        $tabs.forEach(tabs => tabs.classList.remove('active'))
 
-        $granReserva.forEach(granReserva =>{
-            if(tabVal === 'granReserva'){
-                granReserva.style.display = 'block'
-            }
-        })
+        ev.target.classList.add('active');
 
-        $specialCollection.forEach(specialCollection =>{
-            if(tabVal === 'specialCollection'){
-                specialCollection.style.display = 'block'
-            }
-        })
+        const tabVal = ev.target.getAttribute('data-tabs');
 
-        $all.forEach(all => {
-            if(tabVal === 'all'){
-                all.style.display = 'block'
-            }
-        })
+        $all.forEach(all => tabVal === 'all' ? all.style.display = 'block' : all.style.display = 'none')
 
-       })
-   })
-  
-  //search by typing
+        tabVal === 'reserva' ? $reserva.forEach(reserva => reserva.style.display = 'block') : null;
 
-   const $searchBar = document.getElementById('searchBar')
-   const $ul = document.getElementById('containerWines')
+        tabVal === 'granReserva' ? $granReserva.forEach(granReserva => granReserva.style.display = 'block') : null;
 
-   $searchBar.addEventListener('keyup', (e) =>{
-       const txt = e.target.value.toLowerCase();
-       const wines = $ul.getElementsByTagName('li')
+        tabVal === 'specialCollection' ? $specialCollection.forEach(specialCollection => specialCollection.style.display = 'block') : null;
 
-       Array.from(wines).forEach( wine => {
-           const type = wine.dataset.item
-           
-           if(type.toLowerCase().indexOf(txt) != -1){
-               wine.style.display = 'block'
-           } else {
-               wine.style.display = 'none'
-           }
-       })
-       
-    
     })
+})
 
 
+const $searchBar = document.getElementById('searchBar')
 
-})();
+$searchBar.addEventListener('keyup', (ev) => {
+    const txt = ev.target.value.toLowerCase();
+    const wines = document.querySelectorAll('#containerWines li')
 
-
+    wines.forEach(wine => {
+        const wineName = wine.dataset.item
+        wineName.toLowerCase().indexOf(txt) !== -1 ? wine.style.display = 'block' : wine.style.display = 'none'
+    })
+})
 
 
 
